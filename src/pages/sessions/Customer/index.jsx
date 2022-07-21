708c79ff8c5122ef8dc62d71fccb5cd32b2500f5
chamodisamarasinghe <chamodisandunika98@gmail.com>
2022-07-21 18:27:33 +0530

    Updated file

--- src/pages/sessions/Customer/index.jsx
+++ src/pages/sessions/Customer/index.jsx
@@ -1,14 +1,9 @@
-import { Typography } from "@mui/material";
-import { withStyles } from "@mui/styles";
 import React, { Component, Fragment } from "react";
-import { styleSheet } from "./style";
-import TextField from '@mui/material/TextField';
-import Grid from '@mui/material/Grid';
-import Autocomplete from '@mui/material/Autocomplete';
-import GDSEButton from "../../../components/common/Button"
-import NavBar from "../../../components/common/NavBar";
-import {Link, Route} from "react-router-dom";
-import Button from "@mui/material/Button";
+import { Grid, Typography } from "@mui/material";
+import { TextValidator, ValidatorForm } from "react-material-ui-form-validator";
+import GDSEButton from "../../../components/common/Button";
+import CustomerService from "../../../services/CustomerService";
+import GDSESnackBar from "../../../components/common/SnackBar";
 import Table from '@mui/material/Table';
 import TableBody from '@mui/material/TableBody';
 import TableCell from '@mui/material/TableCell';
@@ -16,126 +11,340 @@ import TableContainer from '@mui/material/TableContainer';
 import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
 import Paper from '@mui/material/Paper';
-
-
-
-
-
-
-
+import DeleteIcon from '@mui/icons-material/Delete';
+import IconButton from '@mui/material/IconButton';
+import Tooltip from '@mui/material/Tooltip';
+import EditIcon from '@mui/icons-material/Edit';
 
 class Customer extends Component {
-
-
-
-
-
     constructor(props) {
         super(props);
         this.state = {
-            top100Films: [
-                { label: 'Male' },
-                { label: 'Female' }
+            formData: {
+                id: '',
+                name: '',
+                address: '',
+                salary: ''
+            },
+            alert: false,
+            message: '',
+            severity: '',
 
-            ]
+            data: [],
+            btnLabel: 'save',
+            btnColor: 'primary'
         }
+    }
 
+    deleteCustomer = async (id) => {
+        let params = {
+            id: id
+        }
+        let res = await CustomerService.deleteCustomer(params);
+
+        if(res.status === 200) {
+            this.setState({
+                alert: true,
+                message: res.data.message,
+                severity: 'success'
+            });
+            this.loadData();
+        } else {
+            this.setState({
+                alert: true,
+                message: res.data.message,
+                severity: 'error'
+            });
+        }
+    };
+
+    updateCustomer = (data) => {
+        console.log(data)
+
+        this.setState({
+            btnLabel: 'update',
+            btnColor: 'secondary',
+            formData: {
+                id: data.id,
+                name: data.name,
+                address: data.address,
+                contactNo: data.contactNo,
+                email: data.email,
+                nic: data.nic
+            }
+        });
+    };
+
+    clearFields = () => {
+        this.setState({
+            formData: {
+                id: '',
+                name: '',
+                address: '',
+                contactNo: '',
+                email: '',
+                nic: '',
+
+            }
+        });
+    };
+
+    // ------- React Map function example -------
+    exampleForMap = () => {
+        this.state.data.map((value, index) => {
+            console.log(value)   // access element one by one
+        })
+    };
+
+    loadData = async () => {
+        let res = await CustomerService.fetchCustomer();
+
+        if (res.status === 200) {
+            this.setState({
+                data: res.data.data
+            });
+        }
+        console.log(this.state.data)    // print customers array
+
+        this.exampleForMap()
+
+    };
+
+    submitCustomer = async () => {
+        let formData = this.state.formData;
+
+        if(this.state.btnLabel === "save") {
+            let res = await CustomerService.postCustomer(formData);
+
+            console.log(res)    //print the promise
+
+            if (res.status === 201) {
+                this.setState({
+                    alert: true,
+                    message: res.data.message,
+                    severity: 'success'
+                });
+                this.clearFields();
+                this.loadData();
+            } else {
+                this.setState({
+                    alert: true,
+                    message: res.response.data.message,
+                    severity: 'error'
+                });
+            }
+        } else {
+            let res = await CustomerService.putCustomer(formData);
+            if(res.status === 200) {
+                this.setState({
+                    alert: true,
+                    message: res.data.message,
+                    severity: 'success',
+                    btnLabel: 'save',
+                    btnColor: 'primary'
+                });
+                this.clearFields();
+                this.loadData();
+            } else {
+                this.setState({
+                    alert: true,
+                    message: res.response.data.message,
+                    severity: 'error'
+                });
+            }
+        }
+    };
+
+    componentDidMount() {
+        this.loadData();
     }
 
     render() {
-
-
-
-
-        const { classes } = this.props;
         return (
-            <Fragment>
+            // <Fragment>
+            <>
+                <ValidatorForm ref="form" className="pt-2" onSubmit={this.submitCustomer} >
+                    <Grid container className="pt-2" spacing={3}>
+                        <Grid item lg={12} xs={12} sm={12} md={12}>
+                            <Typography variant="h2">Customer Manage</Typography>
+                        </Grid>
+                        <Grid item xs={12} sm={12} md={6} lg={6}>
+                            <Typography variant="subtitle1">Customer Id</Typography>
+                            <TextValidator
+                                id="outlinedbasic"
+                                placeholder="Customer Id"
+                                variant="outlined"
+                                size="small"
+                                style={{ width: '100%' }}
+                                value={this.state.formData.id}
+                                onChange={(e) => {
+                                    let formData = this.state.formData
+                                    formData.id = e.target.value
+                                    this.setState({ formData })
+                                }}
+                                validators={['required']}
+                            />
+                        </Grid>
+                        <Grid item xs={12} sm={12} md={6} lg={6}>
+                            <Typography variant="subtitle1">Customer Name</Typography>
+                            <TextValidator
+                                id="outlinedbasic"
+                                placeholder="Customer Name"
+                                variant="outlined"
+                                size="small"
+                                style={{ width: '100%' }}
+                                value={this.state.formData.name}
+                                onChange={(e) => {
+                                    let formData = this.state.formData
+                                    formData.name = e.target.value
+                                    this.setState({ formData })
+                                }}
+                                validators={['required']}
+                            />
+                        </Grid>
+                        <Grid item xs={12} sm={12} md={6} lg={6}>
+                            <Typography variant="subtitle1">Customer Address</Typography>
+                            <TextValidator
+                                id="outlinedbasic"
+                                placeholder="Customer Address"
+                                variant="outlined"
+                                size="small"
+                                style={{ width: '100%' }}
+                                value={this.state.formData.address}
+                                onChange={(e) => {
+                                    let formData = this.state.formData
+                                    formData.address = e.target.value
+                                    this.setState({ formData })
+                                }}
+                                validators={['required']}
+                            />
+                        </Grid>
+                        <Grid item xs={12} sm={12} md={6} lg={6}>
+                            <Typography variant="subtitle1">Contact No</Typography>
+                            <TextValidator
+                                id="outlinedbasic"
+                                placeholder="Contact No"
+                                variant="outlined"
+                                size="small"
+                                style={{ width: '100%' }}
+                                value={this.state.formData.contactNo}
+                                onChange={(e) => {
+                                    let formData = this.state.formData
+                                    formData.contactNo = e.target.value
+                                    this.setState({ formData })
+                                }}
+                                validators={['required']}
+                            />
+                        </Grid>
+
+                        <Grid item xs={12} sm={12} md={6} lg={6}>
+                            <Typography variant="subtitle1">Email</Typography>
+                            <TextValidator
+                                id="outlinedbasic"
+                                placeholder="Email"
+                                variant="outlined"
+                                size="small"
+                                style={{ width: '100%' }}
+                                value={this.state.formData.email}
+                                onChange={(e) => {
+                                    let formData = this.state.formData
+                                    formData.email = e.target.value
+                                    this.setState({ formData })
+                                }}
+                                validators={['required']}
+                            />
+                        </Grid>
+
+                        <Grid item xs={12} sm={12} md={6} lg={6}>
+                            <Typography variant="subtitle1">NIC</Typography>
+                            <TextValidator
+                                id="outlinedbasic"
+                                placeholder="NIC"
+                                variant="outlined"
+                                size="small"
+                                style={{ width: '100%' }}
+                                value={this.state.formData.nic}
+                                onChange={(e) => {
+                                    let formData = this.state.formData
+                                    formData.nic = e.target.value
+                                    this.setState({ formData })
+                                }}
+                                validators={['required']}
+                            />
+                        </Grid>
 
 
-
-                <div>
-                    <Link to="/" style={{position:'absolute', left:1100, textDecoration:"none" ,color:'black' }}><NavBar disabled={false} disableFocusRipple={false} disableRipple={false}  iconPosition='top' label='Home' wrapped={false} /></Link>
-                    <Link to="customer" style={{position:'absolute', left:1200, textDecoration:"none" ,color:'black' }}><NavBar disabled={false} disableFocusRipple={false} disableRipple={false} iconPosition='top' label='Customer' wrapped={false} /></Link>
-                    <Link to="item" style={{position:'absolute', left:1300, textDecoration:"none" ,color:'black' }}><NavBar disabled={false} disableFocusRipple={false} disableRipple={false}  iconPosition='top' label='Item' wrapped={false} /></Link>
-                </div>
-
-                <div className={classes.title__container}>
-                    <Typography variant="h4">
-                        Customer Manage
-                    </Typography>
-                </div>
-
-                <Grid container spacing={0.5}>
-                    <Grid item lg={6} md={6} sm={6} xm={6} >
-                        <TextField id="outlined-basic" placeHolder="Customer Id" label="Customer Id" variant="outlined" size="small"
-                                   style={{width: '90%', top:150}} />
+                        <Grid container style={{ marginTop: '10px' }} direction="row" justifyContent="flex-end" alignItems="center">
+                            <GDSEButton label={this.state.btnLabel} type="submit" size="small" color={this.state.btnColor} variant="contained"/>
+                        </Grid>
                     </Grid>
-
-                    <Grid item lg={6} md={6} sm={6} xm={6} >
-                        <TextField id="outlined-basic" placeHolder="Name" label="Name" variant="outlined" size="small"
-                                   style={{width: '90%', top:180}}/>
-                    </Grid>
-                    <Grid item lg={6} md={6} sm={6} xm={6}>
-                        <TextField id="outlined-basic" placeHolder="Address" label="Address" variant="outlined" size="small"
-                                   style={{width: '90%', top:180}}
-                        />
-                    </Grid>
-
-                    <Grid item lg={6} md={6} sm={6} xm={6}>
-                        <TextField id="outlined-basic" placeHolder="Contact No" label="Contact No" variant="outlined" size="small"
-                                   style={{width: '90%', top:180}}
-                        />
-                    </Grid>
-
-                    <Grid item lg={6} md={6} sm={6} xm={6}>
-                        <TextField id="outlined-basic" placeHolder="Email" label="Email" variant="outlined" size="small"
-                                   style={{width: '90%', top:180}}
-                        />
-                    </Grid>
-
-                    <Grid item lg={6} md={6} sm={6} xm={6}>
-                        <TextField id="outlined-basic" placeHolder="NIC" label="NIC" variant="outlined" size="small"
-                                   style={{width: '90%', top:180}}
-                        />
-                    </Grid>
-
-
-
-
-
-
-
-
-
-
-
-                    <Grid item lg={12} md={12} sm={12} xm={12} style={{display: 'flex', position:"absolute", top:270, left:1000, width:300,color:"success"}} justifyContent="flex-end" >
-                        <GDSEButton size="large" variant="contained" label="save"/>
-
-                        <Button variant="contained" color="success" style={{left:50}}>
-                            Update
-                        </Button>
-                        <Button variant="contained" color="error" style={{left:100}}>
-                            Cancel
-                        </Button>
-
-
-
-                    </Grid>
+                </ValidatorForm>
+                <Grid contaner style={{ marginTop: '15px' }}>
+                    <TableContainer component={Paper}>
+                        <Table sx={{ minWidth: 650 }} aria-label="customer table">
+                            <TableHead>
+                                <TableRow>
+                                    <TableCell align="left">Customer Id</TableCell>
+                                    <TableCell align="left">Customer Name</TableCell>
+                                    <TableCell align="left">Customer Address</TableCell>
+                                    <TableCell align="left">Contact No</TableCell>
+                                    <TableCell align="left">Email</TableCell>
+                                    <TableCell align="left">NIC</TableCell>
+                                    <TableCell align="left">Action</TableCell>
+                                </TableRow>
+                            </TableHead>
+                            <TableBody>
+                                {
+                                    this.state.data.map((row) => (
+                                        <TableRow>
+                                            <TableCell align="left">{row.id}</TableCell>
+                                            <TableCell align="left">{row.name}</TableCell>
+                                            <TableCell align="left">{row.address}</TableCell>
+                                            <TableCell align="left">{row.contactNo}</TableCell>
+                                            <TableCell align="left">{row.email}</TableCell>
+                                            <TableCell align="left">{row.nic}</TableCell>
+                                            <TableCell align="left">
+                                                <Tooltip title="Edit">
+                                                    <IconButton
+                                                        onClick={() => {
+                                                            console.log("edit icon clicked!")
+                                                            this.updateCustomer(row);
+                                                        }}
+                                                    >
+                                                        <EditIcon color="primary" />
+                                                    </IconButton>
+                                                </Tooltip>
+                                                <Tooltip title="Delete">
+                                                    <IconButton
+                                                        onClick={() => {
+                                                            this.deleteCustomer(row.id)
+                                                        }}
+                                                    >
+                                                        <DeleteIcon color="error" />
+                                                    </IconButton>
+                                                </Tooltip>
+                                            </TableCell>
+                                        </TableRow>
+                                    ))
+                                }
+                            </TableBody>
+                        </Table>
+                    </TableContainer>
                 </Grid>
-
-
-
-
-
-
-
-
-
-
-            </Fragment>
-
-        )
+                <GDSESnackBar
+                    open={this.state.alert}
+                    onClose={() => {
+                        this.setState({ alert: false })
+                    }}
+                    message={this.state.message}
+                    autoHideDuration={3000}
+                    severity={this.state.severity}
+                    variant="filled"
+                />
+            </>
+            // </Fragment>
+        );
     }
 }
-export default withStyles(styleSheet)(Customer)
+
+export default Customer;
