dde22f70c280be4b051033c0e69eb36655684ca5
chamodisamarasinghe <chamodisandunika98@gmail.com>
2022-07-22 09:30:13 +0530

    Updated file

--- src/app/App.js
+++ src/app/App.js
@@ -1,14 +1,21 @@
 import React from "react";
 import Login from "../pages/sessions/Login";
 import {Routes, Route} from "react-router-dom";
-import DashBoard from "../pages/sessions/DashBoard";
+import DashBoard from "../components/DashBoard";
 import Customer from "../pages/sessions/Customer"
+import AddVehicle from "../pages/sessions/AddVehicle"
+import Sidebar from "../components/Sidebar"
+import SignUp from "../pages/sessions/SignUp"
 
 function App() {
     return (
+
         <Routes>
             <Route exact path='/' element={<DashBoard/>}/>
             <Route path="customer" element={<Customer/>}/>
+            <Route path="addVehicle" element={<AddVehicle/>}/>
+            <Route path="sidebar" element={<Sidebar/>}/>
+            <Route path="signup" element={<SignUp/>}/>
         </Routes>
     );
 }
