b6a227e494918a12cd49580d9e83b5beb3100123
chamodisamarasinghe <chamodisandunika98@gmail.com>
2022-07-24 00:34:19 +0530

    Updated file

--- src/pages/sessions/Customer/index.jsx
+++ src/pages/sessions/Customer/index.jsx
@@ -176,9 +176,10 @@ class Customer extends Component {
                 <ValidatorForm  ref="form" className="pt-2" onSubmit={this.submitCustomer} >
                     <Grid >
                     <div style={{color:"red"}}>
-                        <Link to="/" style={{position:'absolute', left:1100, top:20, textDecoration:"none" ,color:'black', fontWeight:"bold" }}><NavBar disabled={false} disableFocusRipple={false} disableRipple={false}  iconPosition='top' label='Home' wrapped={false} /></Link>
+                        <Link to="/" style={{position:'absolute', left:1000, top:20, textDecoration:"none" ,color:'black', fontWeight:"bold" }}><NavBar disabled={false} disableFocusRipple={false} disableRipple={false}  iconPosition='top' label='Home' wrapped={false} /></Link>
+                        <Link to="about" style={{position:'absolute', left:1100, textDecoration:"none" ,color:'black',top:20 }}><NavBar disabled={false} disableFocusRipple={false} disableRipple={false} iconPosition='top' label='About us' wrapped={false} /></Link>
                         <Link to="customer" style={{position:'absolute', left:1200, top:20, textDecoration:"none" ,color:'black' }}><NavBar disabled={false} disableFocusRipple={false} disableRipple={false} iconPosition='top' label='Customer' wrapped={false} /></Link>
-                        <Link to="item" style={{position:'absolute', left:1300, top:20, textDecoration:"none" ,color:'black' }}><NavBar disabled={false} disableFocusRipple={false} disableRipple={false}  iconPosition='top' label='Item' wrapped={false} /></Link>
+                        <Link to="driver" style={{position:'absolute', left:1300, textDecoration:"none" ,color:'black',top:20 }}><NavBar disabled={false} disableFocusRipple={false} disableRipple={false}  iconPosition='top' label='Drivers' wrapped={false} /></Link>
                     </div>
                  </Grid>
 
