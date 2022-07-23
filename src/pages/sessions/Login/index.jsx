30b60bac19af4f6859f2ced489553cd1ee97a228
chamodisamarasinghe <chamodisandunika98@gmail.com>
2022-07-23 18:05:49 +0530

    Updated file

--- src/pages/sessions/Login/index.jsx
+++ src/pages/sessions/Login/index.jsx
@@ -6,6 +6,7 @@ import TextField from '@mui/material/TextField';
 import GDSEButton from "../../../components/common/Button";
 import GDSESnackBar from "../../../components/common/SnackBar";
 
+const log = new URL("../../../assets/log.png",import.meta.url)
 class Login extends Component {
     constructor(props) {
         super(props);
@@ -53,7 +54,8 @@ class Login extends Component {
             <div className={classes.container}>
                 <div className={classes.login__cover}>
                     <div className={classes.title__container}>
-                        <Typography variant="h4" style={{ fontFamily: "Times New Roman", fontWeight:"bold"}}>Login</Typography>
+                        <img src={log} style={{position:"absolute", top:150, left:710,width:90,height:90}}/>
+                        <Typography variant="h4" style={{ fontFamily: "Times New Roman", fontWeight:"bold",position:"absolute",top:22,left:700,color:"#00008B"}}>Login</Typography>
                     </div>
                     <div className={classes.form__container}>
                         <TextField
