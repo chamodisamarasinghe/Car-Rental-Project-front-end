25e08ef0c513c1f6b23aa7cfeeb9c2270a5c2ed0
chamodisamarasinghe <chamodisandunika98@gmail.com>
2022-07-24 00:37:00 +0530

    Updated file

--- src/pages/sessions/AddCar/index.jsx
+++ src/pages/sessions/AddCar/index.jsx
@@ -6,6 +6,7 @@ import NavBar from "../../../components/common/NavBar";
 import {Link, Route} from "react-router-dom";
 import ImageList from '@mui/material/ImageList';
 import ImageListItem from '@mui/material/ImageListItem';
+import ImageListItemBar from '@mui/material/ImageListItemBar';
 
 
 
@@ -19,19 +20,74 @@ class DashBoard extends Component {
     render() {
         const itemData = [
             {
-                img: 'https://images.unsplash.com/photo-1551963831-b3b1ca40c98e',
-                title: 'Breakfast',
-            },
-            {
-                img: 'https://images.unsplash.com/photo-1551782450-a2132b4ba21d',
-                title: 'Burger',
-            },
-            {
-                img: 'https://images.unsplash.com/photo-1522770179533-24471fcdba45',
-                title: 'Camera',
-            },
+                img: 'https://cdn.excellenceriviera.com/v7/excellenceriviera.com/wp-content/uploads/2020/06/Luxury-SUV-Car.jpg',
+                title: 'Suzuki Alto - Premium Manual *7',
+                author: 'Daily rate-2500/=',
 
+            },
+            {
+                img: 'https://cdn.excellenceriviera.com/v7/excellenceriviera.com/wp-content/uploads/2020/06/Luxury-SUV-Car.jpg',
+                title: 'Burger',
+                author: '@rollelflex_graphy726',
+            },
+            {
+                img: 'https://cdn.excellenceriviera.com/v7/excellenceriviera.com/wp-content/uploads/2020/06/Luxury-SUV-Car.jpg',
+                title: 'Camera',
+                author: '@helloimnik',
+            },
+            {
+                img: 'https://images.unsplash.com/photo-1444418776041-9c7e33cc5a9c',
+                title: 'Coffee',
+                author: '@nolanissac',
+            },
+            {
+                img: 'https://www.bmw.cc/en/all-models/x-series.html',
+                title: 'Hats',
+                author: '@hjrc33',
+            },
+            {
+                img: 'https://images.unsplash.com/photo-1558642452-9d2a7deb7f62',
+                title: 'Honey',
+                author: '@arwinneil',
+            },
+            {
+                img: 'https://images.unsplash.com/photo-1516802273409-68526ee1bdd6',
+                title: 'Basketball',
+                author: '@tjdragotta',
+            },
+            {
+                img: 'https://images.unsplash.com/photo-1518756131217-31eb79b20e8f',
+                title: 'Fern',
+                author: '@katie_wasserman',
+            },
+            {
+                img: 'https://images.unsplash.com/photo-1597645587822-e99fa5d45d25',
+                title: 'Mushrooms',
+                author: '@silverdalex',
+            },
+            {
+                img: 'https://images.unsplash.com/photo-1567306301408-9b74779a11af',
+                title: 'Tomato basil',
+                author: '@shelleypauls',
+            },
+            {
+                img: 'https://images.unsplash.com/photo-1471357674240-e1a485acb3e1',
+                title: 'Sea star',
+                author: '@peterlaster',
+            },
+            {
+                img: 'https://images.unsplash.com/photo-1589118949245-7d38baf380d6',
+                title: 'Bike',
+                author: '@southside_customs',
+            },
         ];
+
+
+
+
+
+
+
 
         const { classes } = this.props;
         return (
@@ -50,18 +106,31 @@ class DashBoard extends Component {
 
 
 
-                <ImageList sx={{ width: 500, height: 450, position:'absolute', left:400, top:150 }} cols={3} rowHeight={164}>
+
+
+
+                <ImageList sx={{ width: 1500, height: 1200 }}>
                     {itemData.map((item) => (
                         <ImageListItem key={item.img}>
                             <img
-                                src={`${item.img}?w=164&h=164&fit=crop&auto=format`}
-                                srcSet={`${item.img}?w=164&h=164&fit=crop&auto=format&dpr=2 2x`}
+                                src={`${item.img}?w=248&fit=crop&auto=format`}
+                                srcSet={`${item.img}?w=248&fit=crop&auto=format&dpr=2 2x`}
                                 alt={item.title}
                                 loading="lazy"
+                            />
+                            <ImageListItemBar
+                                title={item.title}
+                                subtitle={<span>by: {item.author}</span>}
+                                position="below"
                             />
                         </ImageListItem>
                     ))}
                 </ImageList>
+
+
+
+
+
 
 
 
---
+++ src/pages/sessions/Driver/index.jsx
@@ -0,0 +1,357 @@
+import React, { Component, Fragment } from "react";
+import { Grid, Typography } from "@mui/material";
+import { TextValidator, ValidatorForm } from "react-material-ui-form-validator";
+import GDSEButton from "../../../components/common/Button";
+import CustomerService from "../../../services/CustomerService";
+import GDSESnackBar from "../../../components/common/SnackBar";
+import {Link} from "react-router-dom";
+import NavBar from "../../../components/common/NavBar";
+import TableContainer from "@mui/material/TableContainer";
+import Paper from "@mui/material/Paper";
+import Table from "@mui/material/Table";
+import TableHead from "@mui/material/TableHead";
+import TableRow from "@mui/material/TableRow";
+import TableCell from "@mui/material/TableCell";
+import TableBody from "@mui/material/TableBody";
+import Tooltip from "@mui/material/Tooltip";
+import IconButton from "@mui/material/IconButton";
+import EditIcon from "@mui/icons-material/Edit";
+import DeleteIcon from "@mui/icons-material/Delete";
+
+
+class Driver extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            formData: {
+                licenseNo: '',
+                name: '',
+                address: '',
+                contactNo: '',
+                email: '',
+                nic: ''
+            },
+            alert: false,
+            message: '',
+            severity: '',
+
+            data: [],
+            btnLabel: 'save',
+            btnColor: 'primary'
+        }
+    }
+
+    deleteCustomer = async (id) => {
+        let params = {
+            id: id
+        }
+        let res = await CustomerService.deleteCustomer(params);
+
+        if(res.status === 200) {
+            this.setState({
+                alert: true,
+                message: res.data.message,
+                severity: 'success'
+            });
+            this.loadData();
+        } else {
+            this.setState({
+                alert: true,
+                message: res.data.message,
+                severity: 'error'
+            });
+        }
+    };
+
+    updateCustomer = (data) => {
+        console.log(data)
+
+        this.setState({
+            btnLabel: 'update',
+            btnColor: 'secondary',
+            formData: {
+                id: data.id,
+                name: data.name,
+                address: data.address,
+                contactNo: data.contactNo,
+                email: data.email,
+                nic: data.nic
+            }
+        });
+    };
+
+    clearFields = () => {
+        this.setState({
+            formData: {
+                id: '',
+                name: '',
+                address: '',
+                contactNo: '',
+                email: '',
+                nic: '',
+
+            }
+        });
+    };
+
+    // ------- React Map function example -------
+    exampleForMap = () => {
+        this.state.data.map((value, index) => {
+            console.log(value)   // access element one by one
+        })
+    };
+
+    loadData = async () => {
+        let res = await CustomerService.fetchCustomer();
+
+        if (res.status === 200) {
+            this.setState({
+                data: res.data.data
+            });
+        }
+        console.log(this.state.data)    // print customers array
+
+        this.exampleForMap()
+
+    };
+
+    submitCustomer = async () => {
+        let formData = this.state.formData;
+
+        if(this.state.btnLabel === "save") {
+            let res = await CustomerService.postCustomer(formData);
+
+            console.log(res)    //print the promise
+
+            if (res.status === 201) {
+                this.setState({
+                    alert: true,
+                    message: res.data.message,
+                    severity: 'success'
+                });
+                this.clearFields();
+                this.loadData();
+            } else {
+                this.setState({
+                    alert: true,
+                    message: res.response.data.message,
+                    severity: 'error'
+                });
+            }
+        } else {
+            let res = await CustomerService.putCustomer(formData);
+            if(res.status === 200) {
+                this.setState({
+                    alert: true,
+                    message: res.data.message,
+                    severity: 'success',
+                    btnLabel: 'save',
+                    btnColor: 'primary'
+                });
+                this.clearFields();
+                this.loadData();
+            } else {
+                this.setState({
+                    alert: true,
+                    message: res.response.data.message,
+                    severity: 'error'
+                });
+            }
+        }
+    };
+
+    componentDidMount() {
+        this.loadData();
+    }
+
+    render() {
+        return (
+
+            // <Fragment>
+            // <Grid style={{backgroundImage: "url(" + Background + ")"}}>
+            <>
+
+                <ValidatorForm  ref="form" className="pt-2" onSubmit={this.submitCustomer} >
+                    <Grid >
+                        <div>
+                            <Link to="/" style={{position:'absolute', left:1000, textDecoration:"none" ,color:'black',top:20 }}><NavBar disabled={false} disableFocusRipple={false} disableRipple={false}  iconPosition='top' label='Home' wrapped={false} /></Link>
+                            <Link to="about" style={{position:'absolute', left:1100, textDecoration:"none" ,color:'black',top:20 }}><NavBar disabled={false} disableFocusRipple={false} disableRipple={false} iconPosition='top' label='About us' wrapped={false} /></Link>
+                            <Link to="customer" style={{position:'absolute', left:1200, textDecoration:"none" ,color:'black',top:20 }}><NavBar disabled={false} disableFocusRipple={false} disableRipple={false} iconPosition='top' label='Customer' wrapped={false} /></Link>
+                            <Link to="driver" style={{position:'absolute', left:1300, textDecoration:"none" ,color:'black',top:20 }}><NavBar disabled={false} disableFocusRipple={false} disableRipple={false}  iconPosition='top' label='Drivers' wrapped={false} /></Link>
+                        </div>
+                    </Grid>
+
+
+
+
+                    <Grid container className="pt-2" spacing={3}>
+                        <Grid item lg={12} xs={12} sm={12} md={12}>
+                            <Typography variant="h3" style={{marginLeft:520, marginTop:10, color:"#00008B", fontWeight:"bold", fontSize:40}}>Driver Manage</Typography>
+                        </Grid>
+
+
+                        <Grid item xs={12} sm={12} md={6} lg={6} >
+                            <Typography style={{top:145, left:65, position:"absolute"}} variant="subtitle1"  >License No</Typography>
+                            <TextValidator
+                                id="outlinedbasic"
+                                placeholder="License No"
+                                variant="outlined"
+                                size="small"
+                                style={{ width: '90%', left:50 ,top:100}}
+                                value={this.state.formData.id}
+                                onChange={(e) => {
+                                    let formData = this.state.formData
+                                    formData.id = e.target.value
+                                    this.setState({ formData })
+                                }}
+                                validators={['required']}
+                            />
+                        </Grid>
+
+
+                        <Grid item xs={12} sm={12} md={6} lg={6} >
+                            <Typography style={{top:145, left:820, position:"absolute"}} variant="subtitle1" >Driver Name</Typography>
+                            <TextValidator
+                                id="outlinedbasic"
+                                placeholder="Driver Name"
+                                variant="outlined"
+                                size="small"
+                                style={{ width: '90%', left:50 ,top:100}}
+                                value={this.state.formData.name}
+                                onChange={(e) => {
+                                    let formData = this.state.formData
+                                    formData.name = e.target.value
+                                    this.setState({ formData })
+                                }}
+                                validators={['required']}
+                            />
+                        </Grid>
+                        <Grid item xs={12} sm={12} md={6} lg={6} >
+                            <Typography style={{top:230, left:65, position:"absolute"}} variant="subtitle1" >Driver Address</Typography>
+                            <TextValidator
+                                id="outlinedbasic"
+                                placeholder="Driver Address"
+                                variant="outlined"
+                                size="small"
+                                style={{ width: '90%', left:50 ,top:120}}
+                                value={this.state.formData.address}
+                                onChange={(e) => {
+                                    let formData = this.state.formData
+                                    formData.address = e.target.value
+                                    this.setState({ formData })
+                                }}
+                                validators={['required']}
+                            />
+                        </Grid>
+                        <Grid item xs={12} sm={12} md={6} lg={6} >
+                            <Typography style={{top:230, left:820, position:"absolute"}} variant="subtitle1" >Contact No</Typography>
+                            <TextValidator
+                                id="outlinedbasic"
+                                placeholder="Contact No"
+                                variant="outlined"
+                                size="small"
+                                style={{ width: '90%', left:50 ,top:120}}
+                                value={this.state.formData.contactNo}
+                                onChange={(e) => {
+                                    let formData = this.state.formData
+                                    formData.contactNo = e.target.value
+                                    this.setState({ formData })
+                                }}
+                                validators={['required']}
+                            />
+                        </Grid>
+
+                        <Grid  item xs={12} sm={12} md={6} lg={6} >
+                            <Typography style={{top:320, left:65, position:"absolute"}} variant="subtitle1" >NIC</Typography>
+                            <TextValidator
+                                id="outlinedbasic"
+                                placeholder="Email"
+                                variant="outlined"
+                                size="small"
+                                style={{ width: '90%', left:50 ,top:150}}
+                                value={this.state.formData.email}
+                                onChange={(e) => {
+                                    let formData = this.state.formData
+                                    formData.email = e.target.value
+                                    this.setState({ formData })
+                                }}
+                                validators={['required']}
+                            />
+                        </Grid>
+
+
+
+
+                        <Grid container style={{ marginTop: '200px', left:200 }} direction="row" justifyContent="flex-end" alignItems="center">
+                            <GDSEButton label={this.state.btnLabel} type="submit" size="large" color={this.state.btnColor} variant="contained"/>
+                        </Grid>
+                    </Grid>
+                </ValidatorForm>
+                <Grid contaner style={{ marginTop: '40px' }}>
+                    <TableContainer component={Paper}>
+                        <Table sx={{ minWidth: 650 }} aria-label="customer table">
+                            <TableHead>
+                                <TableRow>
+                                    <TableCell align="left">License No</TableCell>
+                                    <TableCell align="left">Driver Name</TableCell>
+                                    <TableCell align="left">Driver Address</TableCell>
+                                    <TableCell align="left">Contact No</TableCell>
+                                    <TableCell align="left">NIC</TableCell>
+                                    <TableCell align="left">Action</TableCell>
+                                </TableRow>
+                            </TableHead>
+                            <TableBody>
+                                {
+                                    this.state.data.map((row) => (
+                                        <TableRow>
+                                            <TableCell align="left">{row.licenseNo}</TableCell>
+                                            <TableCell align="left">{row.name}</TableCell>
+                                            <TableCell align="left">{row.address}</TableCell>
+                                            <TableCell align="left">{row.contactNo}</TableCell>
+                                            <TableCell align="left">{row.nic}</TableCell>
+                                            <TableCell align="left">
+                                                <Tooltip title="Edit">
+                                                    <IconButton
+                                                        onClick={() => {
+                                                            console.log("edit icon clicked!")
+                                                            this.updateCustomer(row);
+                                                        }}
+                                                    >
+                                                        <EditIcon color="primary" />
+                                                    </IconButton>
+                                                </Tooltip>
+                                                <Tooltip title="Delete">
+                                                    <IconButton
+                                                        onClick={() => {
+                                                            this.deleteCustomer(row.id)
+                                                        }}
+                                                    >
+                                                        <DeleteIcon color="error" />
+                                                    </IconButton>
+                                                </Tooltip>
+                                            </TableCell>
+                                        </TableRow>
+                                    ))
+                                }
+                            </TableBody>
+                        </Table>
+                    </TableContainer>
+                </Grid>
+                <GDSESnackBar
+                    open={this.state.alert}
+                    onClose={() => {
+                        this.setState({ alert: false })
+                    }}
+                    message={this.state.message}
+                    autoHideDuration={3000}
+                    severity={this.state.severity}
+                    variant="filled"
+                />
+            </>
+            // </Grid>
+            // </Fragment>
+        );
+    }
+}
+
+export default Driver;
