f84557cad0a356f008edb84441a9d945ed36e9b8
chamodisamarasinghe <chamodisandunika98@gmail.com>
2022-07-26 19:17:32 +0530

    Added file

---
+++ src/pages/sessions/RentalRequest/index.jsx
@@ -0,0 +1,19 @@
+import React, { Component, Fragment } from "react";
+
+
+
+class RentalRequest extends Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return (
+            <Fragment>
+                <h1>hello</h1>
+            </Fragment>
+        )
+    }
+}
+
+export default RentalRequest
