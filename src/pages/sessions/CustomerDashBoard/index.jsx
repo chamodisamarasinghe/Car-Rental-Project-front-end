8264349a25ad89d0575e81bcf7ef7394500fd899
chamodisamarasinghe <chamodisandunika98@gmail.com>
2022-07-26 18:49:49 +0530

    Updated file

--- src/pages/sessions/CustomerDashBoard/index.jsx
+++ src/pages/sessions/CustomerDashBoard/index.jsx
@@ -7,6 +7,8 @@ import Button from "@material-ui/core/Button";
 import GDSEButton from "../../../components/common/Button";
 import {PhotoCamera} from "@mui/icons-material";
 import IconButton from "@material-ui/core/IconButton";
+import HomeDashNav from "../Home/homeDashNav";
+import CustomerDashNav from "./customerDashNav";
 
 
 const cus = new URL("../../../assets/cus.png",import.meta.url)
@@ -20,36 +22,37 @@ class CustomerDashBoard extends Component {
             <Fragment>
                 <Grid >
                     <div>
-                        <Link to="/" style={{position:'absolute', left:900, textDecoration:"none" ,color:'black' }}><NavBar disabled={false} disableFocusRipple={false} disableRipple={false}  iconPosition='top' label='Home' wrapped={false} /></Link>
-                        <Link to="aboutus" style={{position:'absolute', left:1000, textDecoration:"none" ,color:'black' }}><NavBar disabled={false} disableFocusRipple={false} disableRipple={false} iconPosition='top' label='About Us' wrapped={false} /></Link>
-                        <Link to="test" style={{position:'absolute', left:1100, textDecoration:"none" ,color:'black' }}><NavBar disabled={false} disableFocusRipple={false} disableRipple={false} iconPosition='top' label='Cars' wrapped={false} /></Link>
-                        <Link to="customer" style={{position:'absolute', left:1200, textDecoration:"none" ,color:'black' }}><NavBar disabled={false} disableFocusRipple={false} disableRipple={false} iconPosition='top' label='Customer' wrapped={false} /></Link>
-                        <Link to="driver" style={{position:'absolute', left:1300, textDecoration:"none" ,color:'black' }}><NavBar disabled={false} disableFocusRipple={false} disableRipple={false}  iconPosition='top' label='Drivers' wrapped={false} /></Link>
+                        <CustomerDashNav/>
+                    {/*    <Link to="/" style={{position:'absolute', left:900, textDecoration:"none" ,color:'black' }}><NavBar disabled={false} disableFocusRipple={false} disableRipple={false}  iconPosition='top' label='Home' wrapped={false} /></Link>*/}
+                    {/*    <Link to="aboutus" style={{position:'absolute', left:1000, textDecoration:"none" ,color:'black' }}><NavBar disabled={false} disableFocusRipple={false} disableRipple={false} iconPosition='top' label='About Us' wrapped={false} /></Link>*/}
+                    {/*    <Link to="test" style={{position:'absolute', left:1100, textDecoration:"none" ,color:'black' }}><NavBar disabled={false} disableFocusRipple={false} disableRipple={false} iconPosition='top' label='Cars' wrapped={false} /></Link>*/}
+                    {/*    <Link to="customer" style={{position:'absolute', left:1200, textDecoration:"none" ,color:'black' }}><NavBar disabled={false} disableFocusRipple={false} disableRipple={false} iconPosition='top' label='Customer' wrapped={false} /></Link>*/}
+                    {/*    <Link to="driver" style={{position:'absolute', left:1300, textDecoration:"none" ,color:'black' }}><NavBar disabled={false} disableFocusRipple={false} disableRipple={false}  iconPosition='top' label='Drivers' wrapped={false} /></Link>*/}
                     </div>
                 </Grid>
 
 
                 <Grid item lg={12} xs={12} sm={12} md={12}>
-                    <Typography variant="h3" style={{marginLeft:520, marginTop:30, color:"#00008B", fontWeight:"bold", fontSize:40,fontFamily: "Times New Roman"}}>Customer</Typography>
+                    <Typography variant="h3" style={{marginLeft:590, marginTop:30, color:"#00008B", fontWeight:"bold", fontSize:60,fontFamily: "Times New Roman"}}>Customer</Typography>
                 </Grid>
 
 
                 <Grid>
-                    <img src={cus} style={{position:"absolute", top:100, left:600,width:100,height:100}}/>
+                    <img src={cus} style={{position:"absolute", top:200, left:670,width:100,height:100}}/>
                 </Grid>
 
 
 
 
-                <Button variant="contained" color="success" style={{width:400,top:200,height:70,left:150}}>
-                   View About Cars
-                </Button>
+                {/*<Button variant="contained" color="success" style={{width:400,top:200,height:70,left:150}}>*/}
+                {/*   View About Cars*/}
+                {/*</Button>*/}
 
-                <Link to= "customer" underline="none">
-                <Button variant="contained" color="success" style={{width:400,top:200,height:70,left:500}}>
-                    Register
-                </Button>
-                </Link>
+                {/*<Link to= "/" underline="none">*/}
+                {/*<Button variant="contained" color="success" style={{width:400,top:200,height:70,left:500}}>*/}
+                {/*    Register*/}
+                {/*</Button>*/}
+                {/*</Link>*/}
 
                 {/*<Button variant="contained" color="success" style={{width:400,top:400,height:70,left:100}}>*/}
                 {/*    Calculate Income*/}
