8576106150b9bd78ee4c7824b0a1172fafa4f361
chamodisamarasinghe <chamodisandunika98@gmail.com>
2022-07-26 19:10:08 +0530

    Added file

---
+++ src/pages/sessions/Income/index.jsx
@@ -0,0 +1,19 @@
+import React, { Component, Fragment } from "react";
+
+
+
+class Income extends Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return (
+            <Fragment>
+                <h1>hello</h1>
+            </Fragment>
+        )
+    }
+}
+
+export default Income
