7ad18d35ac8e10dc5f2be05e2b8f26ff10650861
chamodisamarasinghe <chamodisandunika98@gmail.com>
2022-07-28 01:10:15 +0530

    Added file

--- src/app/App.js
+++ src/app/App.js
@@ -1,12 +1,12 @@
 import React from "react";
 import {Routes, Route} from "react-router-dom";
-import DashBoard from "../pages/sessions/DashBoard";
-import Customer from "../pages/sessions/Customer"
+import DashBoard from "../pages/sessions/DriverDashBoard";
+import Customer from "../pages/sessions/ManageCustomer"
 
 import AddCar from "../pages/sessions/AddCar"
 import Sidebar from "../components/Sidebar"
 import SignUp from "../pages/sessions/SignUp"
-import DriverDetails from "../pages/sessions/Driver Details"
+import DriverDetails from "../pages/sessions/ViewDriverDetails"
 import Home from "../pages/sessions/Home";
 import Driver from "../pages/sessions/Driver";
 import AboutCars from "../pages/sessions/About Cars";
@@ -21,20 +21,23 @@ import RentalRequest from "../pages/sessions/RentalRequest";
 import Income from "../pages/sessions/Income";
 import ViewRequest from "../pages/sessions/ViewRequest";
 import CustomerSignUp from "../pages/sessions/CustomerSignUp";
+import ViewDriverDetails from "../pages/sessions/ViewDriverDetails";
+import DriverDashBoard from "../pages/sessions/DriverDashBoard";
+import DashDriver from "../pages/sessions/DashDriver";
+import ViewSchedule from "../pages/sessions/ViewDriverDetails";
 
 function App() {
     return (
 
         <Routes>
             <Route exact path='/' element={<Home/>}/>
-            <Route path="dashboard" element={<DashBoard/>}/>
             <Route path="customer" element={<Customer/>}/>
             <Route path="driver" element={<Driver/>}/>
             <Route path="addcar" element={<AddCar/>}/>
             <Route path="about" element={<AboutCars/>}/>
             <Route path="sidebar" element={<Sidebar/>}/>
             <Route path="customersignup" element={<CustomerSignUp/>}/>
-            <Route path="details" element={<DriverDetails/>}/>
+            {/*<Route path="details" element={<DriverDetails/>}/>*/}
             <Route path="aboutus" element={<AboutUs/>}/>
             <Route path="test" element={<Test/>}/>
             <Route exact path="admindashboard" element={<AdminDashBoard/>}/>
@@ -45,6 +48,8 @@ function App() {
             <Route path="rentalrequest" element={<RentalRequest/>}/>
             <Route path="income" element={<Income/>}/>
             <Route path="viewRequest" element={<ViewRequest/>}/>
+            <Route path="dashdriver" element={<DashDriver/>}/>
+            <Route path="viewSchedule" element={<ViewDriverDetails/>}/>
         </Routes>
     );
 }
