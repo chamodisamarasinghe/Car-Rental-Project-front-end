2344852e9ae59b131fdb9cb1dcb84f63e295e3a4
chamodisamarasinghe <chamodisandunika98@gmail.com>
2022-07-27 12:54:48 +0530

    Added file

---
+++ src/pages/sessions/CustomerDashBoard/rentNav.jsx
@@ -0,0 +1,41 @@
+import * as React from 'react';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import IconButton from '@mui/material/IconButton';
+import MenuIcon from '@mui/icons-material/Menu';
+import {Link} from "@mui/material";
+import Button from "@mui/material/Button";
+import Stack from '@mui/material/Stack';
+
+export default function RentDashNav() {
+    return (
+        <Box sx={{flexGrow: 1}}>
+            <AppBar position="static">
+                <Toolbar variant="dense">
+                    <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+                        Easy Car Rental
+                    </Typography>
+
+
+                    <Link href="/" underline="none">
+                        <Button  color='primary' variant="contained">Home</Button>
+                    </Link>
+
+
+
+                    <Link href="test" underline="none">
+                        <Button  color='primary' variant="contained" sx={{ml:2}}>View About Cars</Button>
+                    </Link>
+
+                    <Link href="rentalrequest" underline="none">
+                        <Button  color='primary' variant="contained" sx={{ml:2}}>Rent a Car</Button>
+                    </Link>
+
+
+                </Toolbar>
+            </AppBar>
+        </Box>
+    );
+}
